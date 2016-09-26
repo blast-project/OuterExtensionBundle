cc3c76cc4c15c0284a3b87dba498cd8440449624
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-09-26 11:23:28 +0200

    Refactoring: remove extenallyLinkedClasses property (not used anymore)

--- Entity/Traits/OuterExtensible.php
+++ Entity/Traits/OuterExtensible.php
@@ -7,23 +7,9 @@ use Librinfo\CoreBundle\Tools\Reflection\ClassAnalyzer;
 trait OuterExtensible
 {
     /**
-     * @var array
-     */
-    protected $externallyLinkedClasses = [];
-
-    /**
-     * @return array
-     */
-    public static function getExternallyLinkedClasses()
-    {
-        $instance = new self();
-        return $instance->externallyLinkedClasses;
-    }
-
-    /**
      * If $this has a trait called HasMyEntities, calls $this->initMyEntities
      */
-    protected function initExternallyLinkedClasses()
+    protected function initOuterExtendedClasses()
     {
         $traits = ClassAnalyzer::getTraits($this);
         foreach ( $traits as $trait ) {
